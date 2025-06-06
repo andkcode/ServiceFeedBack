addf847d1f4e12941acd5b4be92ed4483cee00fe
andkcode <akiricuk37@gmail.com>
2025-06-06 20:37:50 +0200

    feat(home): Center FormComponent on Home page
    This commit centers the FormComponent both horizontally and vertically on the Home page.
    
    The previous layout lacked a defined alignment, leading to the component being positioned at the top left. This change improves the visual presentation and user experience by placing the form in a more prominent and balanced location on the page.
    
    The following CSS classes were added to the main div:
    - `items-center`: Centers content horizontally.
    - `justify-center`: Centers content vertically.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ import FormComponent from '../components/FormComponent';
 export default function Home() {
 
   return (
-    <div className="flex flex-col min-h-screen w-full">
+    <div className="flex flex-col min-h-full w-full items-center justify-center">
     <FormComponent/>
     </div>
   )
