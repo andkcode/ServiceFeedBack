6e9e16fec4d8639a6e845b8c2077646460451bf9
andkcode <akiricuk37@gmail.com>
2025-06-06 20:42:23 +0200

    feat(form): Enhance feedback form with textarea and styling
    This commit introduces the following changes to the feedback form:
    
    - Replaced the input field with a textarea to allow for multi-line feedback.
    - Added placeholder text to the textarea to guide the user.
    - Implemented basic styling for the textarea, including padding, border, rounded corners and disabling resizing.
    - Aligned text to the top of the textarea.
    
    These changes improve the user experience by providing a more intuitive and flexible way to submit feedback.

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -8,8 +8,13 @@ export default function FormComponent() {
     <div className="flex flex-col min-h-full w-full items-center justify-center">
     <form className="w-full flex flex-col items-center justify-center gap-4">
     <h3 className='text-2xl'>Give us your feedback!</h3>
-    <input type="text" name="textArea" id="textArea" className='w-[40rem] h-[10rem] bg-gray-700 text-white pl-0 pt-0 text-left border-1 border-gray-400 focus:border-gray-500 rounded-xl' />
-    {/* <StarComponent /> */}
+    <textarea 
+    name="textArea" 
+    id="textArea" 
+    className="w-[40rem] h-[10rem] text-white text-left align-top p-4 border border-gray-400 rounded-xl resize-none" 
+    placeholder="Type your feedback here..."
+        />
+    <StarComponent />
     <button>Send Feedback</button>
 </form>
     </div>
