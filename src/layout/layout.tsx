5ff239d59e49f402bc06374dda96e4cb47cb1ebe
andkcode <akiricuk37@gmail.com>
2025-06-08 01:45:56 +0200

    feat(layout): Implement React Toastify for global notifications
    This commit introduces React Toastify for displaying non-blocking, user-friendly notifications across the application.
    
    The ToastContainer component is added to the main layout to manage toast display.
    A basic example button and notification are added for testing purposes.
    Future commits will integrate Toastify with specific application events.

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -2,13 +2,27 @@ import '../styles/index.css';
 import Header from './Header';
 import Footer from "./Footer";
 import { Outlet } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
 
 export default function AppLayout() {
+  const notify = () => toast("Wow so easy!");
   return (
     <div className="flex flex-col min-h-screen w-full bg-black text-white">
       <Header />
-
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={true}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+        />
       <main className="flex-grow flex w-full items-center justify-center px-4 min-h-full">
+        <button onClick={notify}>Notify!</button>
           <Outlet />
       </main>
 
