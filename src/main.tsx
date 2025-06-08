edb982f66fac139cab8ac32604c1a7102b74ffe7
andkcode <akiricuk37@gmail.com>
2025-06-08 02:49:11 +0200

    feat(auth): Implement AuthProvider to manage user authentication state
    This commit introduces the `AuthProvider` component, which wraps the `RouterProvider` to manage the application's authentication state. This allows components throughout the application to access and update the current user's authentication status.
    
    The `AuthProvider` utilizes React Context to provide a centralized and efficient way to handle authentication logic, avoiding the need to pass authentication props down through multiple component layers. This change sets the stage for implementing features such as login, logout, and protected routes.

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import { AuthProvider } from "./context/AuthContext";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./route/router";
 import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
+  <AuthProvider>
       <RouterProvider router={router} />
-    </React.StrictMode>
+  </AuthProvider>
 );
